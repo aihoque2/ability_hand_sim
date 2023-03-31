36434b3680993d9d0fb8c5cd60da36f493bd9228
aihoque2 <aihoque2@illinois.edu>
2023-03-30 21:05:43 -0700

    control publisher created

---
+++ src/control_hand_test.cpp
@@ -0,0 +1,44 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/float64_multi_array.hpp"
+
+std::vector<double> neutral(10, 0.0);
+
+class JointPub : public rclcpp::Node{
+    public:
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub;
+        std::shared_ptr<std_msgs::msg::Float64MultiArray> msg;
+        rclcpp::TimerBase::SharedPtr timer;
+        int idx; //idx for picking which joint to move
+
+        JointPub() : Node("test_hand_controls"){
+            using namespace std::chrono_literals;
+
+            pub = this->create_publisher<std_msgs::msg::Float64MultiArray>("joint_effort_controller/commands", 10);
+            timer = this->create_wall_timer(500ms, std::bind(&JointPub::timer_callback, this));
+
+            msg = std::make_shared<std_msgs::msg::Float64MultiArray>();
+            msg->data = std::vector<double>(10, 0.0);
+
+            idx = 0;
+        }
+
+        void timer_callback(){
+            if (idx == 10) idx = 0;
+            msg->data[idx] = 0.15;
+            pub->publish(*msg);
+            msg->data[idx] = 0.0;
+            idx++;
+        }   
+};
+
+int main(int argc, char* argv[]){
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<JointPub>());
+    rclcpp::shutdown();
+    return 0;
+}
